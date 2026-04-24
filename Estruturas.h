5e15e1426915663831186d8c9f4520f5ddb8f3c9
fernando-sebastiao <fernandosebastiao888@gmail.com>
2026-04-24 11:55:18 +0100

    adiconado a estruta de cadeiras para permitir um estudante ser inscrito em mais de uma cadeira

--- Estruturas.h
+++ Estruturas.h
@@ -1,10 +1,19 @@
 
 typedef struct
 {
+	char nome[20];
+}Cadeira;
+
+typedef struct
+{
 	int Numero_estudante;
 	int Num_cadeiras;
 	int Ano_frequencia;
 	char nome[20];
+
+	Cadeira *cadeiras;
+	int num_cadeiras;
+	int cap_cadeiras;
 } Estudante;
 
 
